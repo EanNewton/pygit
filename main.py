719140d692885f463f5c71489212d792ca31737b
EanNewton <eannewton5@gmail.com>
2022-07-04 20:37:54 -0700

    Upload files

--- main.py
+++ main.py
@@ -1,12 +1,53 @@
 # initiliaze repo
 import collections
+import difflib
+import enum
 import hashlib
+import operator
 import os.path
 import stat
 import struct
+import sys
 import time
 import urllib.request
 import zlib
+
+# Data for one entry in the git index (.git/index)
+IndexEntry = collections.namedtuple('IndexEntry', [
+    'ctime_s', 'ctime_n', 'mtime_s', 'mtime_n', 'dev', 'ino', 'mode',
+    'uid', 'gid', 'size', 'sha1', 'flags', 'path',
+])
+
+
+class ObjectType(enum.Enum):
+    """
+    Object type enum. There are other types too, but we don't need them.
+    See 'enum object_type' in git's source (git/cache.h)
+    """
+    commit = 1
+    tree = 2
+    blob = 3
+
+
+def read_file(path):
+    """
+    Read contents of file at given path as bytes.
+    :param path:
+    :return:
+    """
+    with open(path, 'rb') as f:
+        return f.read()
+
+
+def write_file(path, data):
+    """
+    Write data bytes to file at given path.
+    :param path:
+    :param data:
+    :return:
+    """
+    with open(path, 'wb') as f:
+        f.write(data)
 
 
 def init(repo):
@@ -45,13 +86,42 @@ def hash_object(data, obj_type, write=True):
     return sha1
 
 
-# the git index
-# Data for one entry in the git index (.git/index)
-IndexEntry = collections.namedtuple('IndexEntry', [
-    'ctime_s', 'ctime_n', 'mtime_s', 'mtime_n', 'dev', 'ino', 'mode',
-    'uid', 'gid', 'size', 'sha1', 'flags', 'path',
-])
+def cat_file(mode, sha1_prefix):
+    """
+    Write the contents of (or info about) object with given SHA-1 prefix to
+    stdout. If mode is 'commit', 'tree', or 'blob', print raw data bytes of
+    object. If mode is 'size', print the size of the object. If mode is
+    'type', print the type of the object. If mode is 'pretty', print a
+    prettified version of the object.
+    :param mode:
+    :param sha1_prefix:
+    :return:
+    """
+    obj_type, data = read_object(sha1_prefix)
+    if mode in ['commit', 'tree', 'blob']:
+        if obj_type != mode:
+            raise ValueError('expected object type {}, got {}'.format(
+                mode, obj_type
+            ))
+        sys.stdout.buffer.write(data)
+    elif mode == 'size':
+        print(len(data))
+    elif mode == 'type':
+        print(obj_type)
+    elif mode == 'pretty':
+        if obj_type in ['commit', 'blob']:
+            sys.stdout.buffer.write(data)
+        elif obj_type == 'tree':
+            for mode, path, sha1 in read_tree(data=data):
+                type_str = 'tree' if stat.S_ISDIR(mode) else 'blob'
+                print('{:06o} {} {}\t{}'.format(mode, type_str, sha1, path))
+        else:
+            assert False, 'unhandled object type {!r}'.format(obj_type)
+    else:
+        raise ValueError('unexpected mode {!r}'.format(mode))
 
+
+# the git index
 def read_index():
     """
     Read git index file and return list of IndexEntry objects
@@ -65,7 +135,7 @@ def read_index():
     assert digest == data[-20:], 'invalid index checksum'
     signature, version, num_entries = struct.unpack('!4sLL', data[:12])
     assert signature == b'DIRC', \
-            'invalid index signature {}'.format(signature)
+        'invalid index signature {}'.format(signature)
     assert version == 2, 'unknown index version {}'.format(version)
     entry_data = data[12:-20]
     entries = []
@@ -84,6 +154,140 @@ def read_index():
     return entries
 
 
+def ls_files(details=False):
+    """
+    Print list of files in index (including mode, SHA-1, and stage number
+    if "details" is True).
+    :param details:
+    :return:
+    """
+    for entry in read_index():
+        if details:
+            stage = (entry.flags >> 12) & 3
+            print('{:6o} {} {:}\t{}'.format(
+                entry.mode, entry.sha1.hex(), stage, entry.path
+            ))
+        else:
+            print(entry.path)
+
+
+def status():
+    """
+    Show status of working copy.
+    :return:
+    """
+    changed, new, deleted = get_status()
+    if changed:
+        print('changed files:')
+        for path in changed:
+            print('    ', path)
+    if new:
+        print('new files:')
+        for path in new:
+            print('    ', path)
+    if deleted:
+        print('deleted files:')
+        for path in deleted:
+            print('    ', path)
+
+
+def get_status():
+    """
+    Get status of working copy, return tuple of (changed_paths, new_paths,
+    deleted_paths).
+    :return:
+    """
+    paths = set()
+    for root, dirs, files in os.walk('.'):
+        dirs[:] = [d for d in dirs if d != '.git']
+        for file in files:
+            path = os.path.join(root, files)
+            path = path.replace('\\', '/')
+            if path.startswith('./'):
+                path = path[2:]
+            paths.add(path)
+    entries_by_path = {e.path: e for e in read_index()}
+    entry_paths = set(entries_by_path)
+    changed = {p for p in (paths & entry_paths)
+               if hash_object(read_file(p), 'blob', write=False) !=
+               entries_by_path[p].sha1.hex()}
+    new = paths - entry_paths
+    deleted = entry_paths - paths
+    return (sorted(changed), sorted(new), sorted(deleted))
+
+
+def diff():
+    """
+    Show diff of files changed (between index and working copy).
+    :return:
+    """
+    changed, _, _ = get_status()
+    entries_by_path = {e.path: e for e in read_index()}
+    for i, path in enumerate(changed):
+        sha1 = entries_by_path[path].sha1.hex()
+        obj_type, data = read_object(sha1)
+        assert obj_type == 'blob'
+        index_lines = data.decode().splitlines()
+        working_lines = read_file(path).decode().splitlines()
+        diff_lines = difflib.unified_diff(
+            index_lines, working_lines,
+            '{} (index)'.format(path),
+            '{} (working copy)'.format(path),
+            lineterm=''
+        )
+        for line in diff_lines:
+            print(line)
+        if i < len(changed) - 1:
+            print('-' * 70)
+
+
+def write_index(entries):
+    """
+    Write list of IndexEntry objects to git index file.
+    :param entries:
+    :return:
+    """
+    packed_entries = []
+    for entry in entries:
+        entry_head = struct.pack(
+            '!LLLLLLLLLL20sH',
+            entry.ctime_s, entry.ctime_n, entry.mtime_s, entry.mtime_n,
+            entry.dev, entry.ino, entry.mode, entry.uid, entry.gid,
+            entry.size, entry.sha1, entry.flags)
+        path = entry.path.encode()
+        length = ((62 + len(path) + 8) // 8) * 8
+        packed_entry = entry_head + path + b'\x00' * (length - 62 - len(path))
+        packed_entries.append(packed_entry)
+    header = struct.pack('!4sLL', b'DIRC', 2, len(entries))
+    all_data = header = b''.join(packed_entries)
+    digest = hashlib.sha1(all_data).digest()
+    write_file(os.path.join('.git', 'index'), all_data + digest)
+
+
+def add(paths):
+    """
+    Add all file paths to git index.
+    :param paths:
+    :return:
+    """
+    paths = [p.replace('\\', '/') for p in paths]
+    all_entries = read_index()
+    entries = [e for e in all_entries if e.path not in paths]
+    for path in paths:
+        sha1 = hash_object(read_file(path), 'blob')
+        st = os.stat(path)
+        flags = len(path.encode())
+        assert flags < (1 << 12)
+        entry = IndexEntry(
+            int(st.st_ctime), 0, int(st.st_mtime), 0, st.st_dev,
+            st.st_ino, st.st_mode, st.st_uid, st.st_gid, st.st_size,
+            bytes.fromhex(sha1), flags, path
+        )
+        entries.append(entry)
+        entries.sort(key=operator.attrgetter('path'))
+        write_index(entries)
+
+
 # committing
 def write_tree():
     """
@@ -93,11 +297,23 @@ def write_tree():
     tree_entries = []
     for entry in read_index():
         assert '/' not in entry.path, \
-                'currently only supports a single, top level directory'
+            'currently only supports a single, top level directory'
         mode_path = '{:o} {}'.format(entry.mode, entry.path).encode()
         tree_entry = mode_path + b'\x00' + entry.sha1
         tree_entries.append(tree_entry)
     return hash_object(b''.join(tree_entries), 'tree')
+
+
+def get_local_master_hash():
+    """
+    Get current commit hash (SHA-1 string) of local master branch.
+    :return:
+    """
+    master_path = os.path.join('.git', 'refs', 'heads', 'master')
+    try:
+        return read_file(master_path).decode().strip()
+    except FileNotFoundError:
+        return None
 
 
 def commit(message, author):
@@ -211,6 +427,75 @@ def get_remote_master_hash(git_url, username, password):
     return master_sha1.decode()
 
 
+def find_object(sha1_prefix):
+    """
+    Find object with given SHA-1 prefix and return path to object in object
+    store, or raise ValueError if there are no objects or multiple objects
+    with this prefix.
+    :param sha1_prefix:
+    :return:
+    """
+    if len(sha1_prefix) < 2:
+        raise ValueError('hash prefix must be 2 or more characters')
+    obj_dir = os.path.join('.git', 'objects', sha1_prefix[:2])
+    rest = sha1_prefix[2:]
+    objects = [name for name in os.listdir(obj_dir) if name.startswith(rest)]
+    if not objects:
+        raise ValueError('object {!r} not found'.format(sha1_prefix))
+    if len(objects) >= 2:
+        raise ValueError('multiple objects ({}) with prefix {!r}'.format(
+            len(objects), sha1_prefix
+        ))
+    return os.path.join(obj_dir, objects[0])
+
+
+def read_object(sha1_prefix):
+    """
+    Read object with given SHA-1 prefix and return tuple of
+    (object_type, data_type), or raise ValueError if not found.
+    :param sha1_prefix:
+    :return:
+    """
+    path = find_object(sha1_prefix)
+    full_data = zlib.decompress(read_file(path))
+    nul_index = full_data.index(b'\x00')
+    header = full_data[:nul_index]
+    obj_type, size_str = header.decode().split()
+    size = int(size_str)
+    data = full_data[nul_index + 1:]
+    assert size == len(data), 'expected size {}, got {} bytes'.format(
+        size, len(data)
+    )
+    return (obj_type, data)
+
+
+def read_tree(sha1=None, data=None):
+    """
+    Read tree object with given SHA-1 (hex string) or data, and return list
+    of (mode, path, sha1) tuples.
+    :param sha1:
+    :param data:
+    :return:
+    """
+    if sha1 is not None:
+        obj_type, data = read_object(sha1)
+        assert obj_type == 'tree'
+    elif data is None:
+        raise TypeError('must specify "sha1" or "data"')
+    i = 0
+    entries = []
+    for _ in range(1000):
+        end = data.find(b'\x00', i)
+        if end == -1:
+            break
+        mode_str, path = data[i:end].decode().split()
+        mode = int(mode_str, 8)
+        digest = data[end + 1:end + 21]
+        entries.append((mode, path, digest.hex()))
+        i = end + 1 + 20
+    return entries
+
+
 def find_tree_objects(tree_sha1):
     """
     Return set of SHA-1 hashes of all objects in this tree
@@ -305,19 +590,113 @@ def push(git_url, username, password):
     :param git_url:
     :param username:
     :param password:
-    :return: 
+    :return:
     """
     remote_sha1 = get_remote_master_hash(git_url, username, password)
     local_sha1 = get_local_master_hash()
     missing = find_missing_objects(local_sha1, remote_sha1)
     lines = ['{} {} refs/heads/master\x00 report-status'.format(
         remote_sha1 or ('0' * 40), local_sha1).encode()
-    ]
+             ]
     data = build_lines_data(lines) + create_pack(missing)
     url = git_url + '/git-receive-pack'
     response = http_request(url, username, password, data=data)
     lines = extract_lines(response)
     assert lines[0] == b'unpack ok\n', \
-            "expected line 1 b'unpack ok', got: {}".format(lines[0])
+        "expected line 1 b'unpack ok', got: {}".format(lines[0])
+
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    sub_parsers = parser.add_subparsers(dest='command', metavar='command')
+    sub_parsers.required = True
+
+    sub_parser = sub_parsers.add_parser('add',
+                                        help='add file(s) to index')
+    sub_parser.add_argument('paths', nargs='+', metavar='path',
+                            help='path(s) of files to add')
+
+    sub_parser = sub_parsers.add_parser('cat-file',
+                                        help='display contents of object')
+    valid_modes = ['commit', 'tree', 'blob', 'size', 'type', 'pretty']
+    sub_parser.add_argument('mode', choices=valid_modes,
+                            help='object type (commit, tree, blob) or display mode (size, '
+                                 'type, pretty)')
+    sub_parser.add_argument('hash_prefix',
+                            help='SHA-1 hash (or hash prefix) of object to display')
+
+    sub_parser = sub_parsers.add_parser('commit',
+                                        help='commit current state of index to master branch')
+    sub_parser.add_argument('-a', '--author',
+                            help='commit author in format "A U Thor <author@example.com>" '
+                                 '(uses GIT_AUTHOR_NAME and GIT_AUTHOR_EMAIL environment '
+                                 'variables by default)')
+    sub_parser.add_argument('-m', '--message', required=True,
+                            help='text of commit message')
+
+    sub_parser = sub_parsers.add_parser('diff',
+                                        help='show diff of files changed (between index and working '
+                                             'copy)')
+
+    sub_parser = sub_parsers.add_parser('hash-object',
+                                        help='hash contents of given path (and optionally write to '
+                                             'object store)')
+    sub_parser.add_argument('path',
+                            help='path of file to hash')
+    sub_parser.add_argument('-t', choices=['commit', 'tree', 'blob'],
+                            default='blob', dest='type',
+                            help='type of object (default %(default)r)')
+    sub_parser.add_argument('-w', action='store_true', dest='write',
+                            help='write object to object store (as well as printing hash)')
+
+    sub_parser = sub_parsers.add_parser('init',
+                                        help='initialize a new repo')
+    sub_parser.add_argument('repo',
+                            help='directory name for new repo')
+
+    sub_parser = sub_parsers.add_parser('ls-files',
+                                        help='list files in index')
+    sub_parser.add_argument('-s', '--stage', action='store_true',
+                            help='show object details (mode, hash, and stage number) in '
+                                 'addition to path')
+
+    sub_parser = sub_parsers.add_parser('push',
+                                        help='push master branch to given git server URL')
+    sub_parser.add_argument('git_url',
+                            help='URL of git repo, eg: https://github.com/USER/REPO.git')
+    sub_parser.add_argument('-p', '--password',
+                            help='password to use for authentication (uses GIT_PASSWORD '
+                                 'environment variable by default)')
+    sub_parser.add_argument('-u', '--username',
+                            help='username to use for authentication (uses GIT_USERNAME '
+                                 'environment variable by default)')
+
+    sub_parser = sub_parsers.add_parser('status',
+                                        help='show status of working copy')
+
+    args = parser.parse_args()
+    if args.command == 'add':
+        add(args.paths)
+    elif args.command == 'cat-file':
+        try:
+            cat_file(args.mode, args.hash_prefix)
+        except ValueError as error:
+            print(error, file=sys.stderr)
+            sys.exit(1)
+    elif args.command == 'commit':
+        commit(args.message, author=args.author)
+    elif args.command == 'diff':
+        diff()
+    elif args.command == 'hash-object':
+        sha1 = hash_object(read_file(args.path), args.type, write=args.write)
+        print(sha1)
+    elif args.command == 'init':
+        init(args.repo)
+    elif args.command == 'ls-files':
+        ls_files(details=args.stage)
+    elif args.command == 'push':
+        push(args.git_url, username=args.username, password=args.password)
+    elif args.command == 'status':
+        status()
+    else:
+        assert False, 'unexpected command {!r}'.format(args.command)
